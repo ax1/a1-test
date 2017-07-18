c9afcb9c73ecce50ecc126a9303576c5b2f7a222
Angel Rego <angelrego@gmail.com>
2017-07-18 11:34:28 +0200

    add testHtmlUrl(url, chrome, depth) method

--- test/index.js
+++ test/index.js
@@ -3,11 +3,15 @@ const test = require('../lib/index')
 
   !async function() {
     try {
-      const folder = '/home/ubuntu/SOFTWARE/IRIDIUMJS/x/test/html'
-      await test.testWebApp(folder, '/usr/lib/chromium-browser/chromium-browser')
-      assert.ok(true, `folder ${folder} is ok`)
+      // // test local folder
+      // const folder = '/home/ubuntu/SOFTWARE/IRIDIUMJS/x/test/html'
+      // await test.testHtmlFolder(folder, '/usr/lib/chromium-browser/chromium-browser')
+      // assert.ok(true, `folder ${folder} is ok`)
+      // test url
+      const url = 'http://google.com'
+      await test.testHtmlUrl(url, '/usr/lib/chromium-browser/chromium-browser', 0)
+      assert.ok(true, `url ${url} is ok`)
     } catch (e) {
-      console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
       assert.fail(e.toString())
     }
   }()
